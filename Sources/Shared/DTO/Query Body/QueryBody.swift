5c5d1612ee187da2eef5a9f1a7cd2fc495f1242f
Nihed Majdoub <nmajdoub.ext@wedigital.garden>
2021-01-11 23:55:07 +0100

    add public init of Filter & Page structs

--- Sources/Shared/DTO/Query Body/QueryBody.swift
+++ Sources/Shared/DTO/Query Body/QueryBody.swift
@@ -10,17 +10,14 @@ import Foundation
 public struct QueryBody: Codable {
 
     public struct Page: Codable {
-        public let count: Int
+        public let count: Int?
         public var offset: String?
-    }
 
-    public struct Variable: Codable {
-        public var page: Page?
-        public var sortBy: String?
-        public var filter: Filter?
-        public var uuid: String?
-
-        public init() { }
+        public init(count: Int? = nil,
+                    offset: String? = nil) {
+            self.count = count
+            self.offset = offset
+        }
     }
 
     public struct Filter: Codable {
@@ -28,6 +25,33 @@ public struct QueryBody: Codable {
         public var regionId: String?
         public var queryString: String?
         public var userId: String?
+
+        public init(category: String? = nil,
+                    regionId: String? = nil,
+                    queryString: String? = nil,
+                    userId: String? = nil) {
+            self.category = category
+            self.regionId = regionId
+            self.queryString = queryString
+            self.userId = userId
+        }
+    }
+
+    public struct Variable: Codable {
+        public var page: Page?
+        public var filter: Filter?
+        public var sortBy: String?
+        public var uuid: String?
+
+        public init(page: Page? = nil,
+                    filter: Filter? = nil,
+                    sortBy: String? = nil,
+                    uuid: String? = nil) {
+            self.page = page
+            self.filter = filter
+            self.sortBy = sortBy
+            self.uuid = uuid
+        }
     }
 
     public let operationName: String
