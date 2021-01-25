251452e7f9c83ca7a9fce79a961e6f01f968fa64
Nihed Majdoub <nmajdoub.ext@wedigital.garden>
2021-01-25 20:28:29 +0100

    fix typo

--- Sources/Shared/DTO/Authentification/smsCode.swift
+++ Sources/Shared/DTO/Authentification/smsCode.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct SendCodeRequestDTO: Codable {
-    public var haphone_number: String
+    public var phone_number: String
 }
 
 public struct SendCodeResponseDTO: Codable {
