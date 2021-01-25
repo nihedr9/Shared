106c3069f62b8d850bbeb457dfcbad67e7a665f3
Nihed Majdoub <nmajdoub.ext@wedigital.garden>
2021-01-25 14:15:39 +0100

    add send sms DTOs

---
+++ Sources/Shared/DTO/Authentification/SendSms.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nihed Majdoub on 25/01/2021.
+//
+
+import Foundation
+
+public struct SendCodeRequestDTO: Codable {
+    public phone_number: String
+}
+
+public struct SendCodeResponseDTO: Codable {
+    public let codeSent: Bool
+    public let publicChallenge: String
+    public let newUser: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case codeSent = "code_sent"
+        case publicChallenge = "public_challenge"
+        case newUser = "new_user"
+    }
+}
