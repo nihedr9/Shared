8dcde82c9464664b049cc2bee42b327b7ba469c7
Nihed Majdoub <nmajdoub.ext@wedigital.garden>
2021-01-28 20:17:18 +0100

    add userDTO to Tayara namespace

--- Sources/Shared/Tayara/DTO/User/UserProfileResponse.swift
+++ Sources/Shared/Tayara/DTO/User/UserProfileResponse.swift
@@ -7,31 +7,31 @@
 
 import Foundation
 
-public struct UserProfileResponse: Codable {
-    public struct DataDTO: Codable {
-        public let profile: UserDTO
-    }
-    public struct UserDTO: Codable {
-
-        public struct ContactPhoneDTO: Codable {
-            public let e164: String
-            public let national: String
+extension Tayara {
+    public struct UserProfileResponse: Codable {
+        public struct DataDTO: Codable {
+            public let profile: UserDTO
         }
-
-        public let uuid: String
-        public let displayName: String
-        public let email: String?
-        public let firstName: String?
-        public let lastName: String?
-        public let memberSince: String?
-        public let contactPhone: ContactPhoneDTO?
-        public let avatar: ImageDTO?
-        public let __typename: String
+        public struct UserDTO: Codable {
+            
+            public struct ContactPhoneDTO: Codable {
+                public let e164: String
+                public let national: String
+            }
+            
+            public let uuid: String
+            public let displayName: String
+            public let email: String?
+            public let firstName: String?
+            public let lastName: String?
+            public let memberSince: String?
+            public let contactPhone: ContactPhoneDTO?
+            public let avatar: ImageDTO?
+            public let __typename: String
+        }
+        public struct ImageDTO: Codable {
+            public let url: String
+        }
+        public let data: DataDTO
     }
-    public struct ImageDTO: Codable {
-        public let url: String
-    }
-    public let data: DataDTO
 }
-
-
